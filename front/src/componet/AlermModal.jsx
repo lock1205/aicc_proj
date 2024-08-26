b103afa88326600eb0e61adc15dc8fce9d578272
lollipopwyn <rwewine@gmail.com>
2024-08-26 16:54:42 +0900

    나라_0826_css 클래스 이름 변경

--- front/src/componet/AlermModal.jsx
+++ front/src/componet/AlermModal.jsx
@@ -5,9 +5,9 @@ const AlermModal = ({ isOpen, onClose, message }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="modalOverlay" onClick={onClose}>
-      <div className="window">
-        <div className="modalContent" onClick={(e) => e.stopPropagation()}>
+    <div className="am_modalOverlay" onClick={onClose}>
+      <div className="am_window">
+        <div className="am_modalContent" onClick={(e) => e.stopPropagation()}>
           <p>{message}</p>
           <button onClick={onClose}>바로 확인</button>
         </div>
