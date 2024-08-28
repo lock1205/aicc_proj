1ebf51582582292413f3f13bc4fc2fd4017003c3
lollipopwyn <rwewine@gmail.com>
2024-08-28 11:57:56 +0900

    나라_0828_requestMethod에 Common GET Request Function추가

--- front/src/util/requestMethods.js
+++ front/src/util/requestMethods.js
@@ -14,3 +14,13 @@ export async function postRequest(url, options) {
     return response.json();
   });
 }
+
+/* ====== Common GET Request Function ====== */
+export async function getRequest(url) {
+  return await fetch(url).then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  });
+}
