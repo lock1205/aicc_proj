c86ea453b96645d11b0f7da7febbdb70f8bc9145
lollipopwyn <rwewine@gmail.com>
2024-08-28 12:46:35 +0900

    나라_0828_front에 getTasks와 postTasks 연결_웹 페이지에 출력되는 것은 아직 확인 안 함

--- front/src/util/apiUrl.js
+++ front/src/util/apiUrl.js
@@ -1,3 +1,5 @@
 const REGISTER_USER_API_URL = 'http://localhost:8080/register_user';
+const POST_TASKS_API_URL = 'http://localhost:8080/post_tasks';
+const GET_TASKS_API_URL = 'http://localhost:8080/get_tasks';
 
-export { REGISTER_USER_API_URL };
+export { REGISTER_USER_API_URL, POST_TASKS_API_URL, GET_TASKS_API_URL };
