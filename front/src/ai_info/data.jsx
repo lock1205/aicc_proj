676ddafb7ffe5bd3b6bf452400a01cb14494025a
lock1205 <mindheal205@gmail.com>
2024-08-28 10:03:27 +0900

    0828_승훈

---
+++ front/src/ai_info/data.jsx
@@ -0,0 +1,123 @@
+export const features = [
+  {
+    icon: <BotMessageSquare />,
+    text: 'Drag-and-Drop Interface',
+    description:
+      'Easily design and arrange your VR environments with a user-friendly drag-and-drop interface.',
+  },
+  {
+    icon: <Fingerprint />,
+    text: 'Multi-Platform Compatibility',
+    description:
+      'Build VR applications that run seamlessly across multiple platforms, including mobile, desktop, and VR headsets.',
+  },
+  {
+    icon: <ShieldHalf />,
+    text: 'Built-in Templates',
+    description:
+      'Jumpstart your VR projects with a variety of built-in templates for different types of applications and environments.',
+  },
+  {
+    icon: <BatteryCharging />,
+    text: 'Real-Time Preview',
+    description:
+      'Preview your VR application in real-time as you make changes, allowing for quick iterations and adjustments.',
+  },
+  {
+    icon: <PlugZap />,
+    text: 'Collaboration Tools',
+    description:
+      'Work together with your team in real-time on VR projects, enabling seamless collaboration and idea sharing.',
+  },
+  {
+    icon: <GlobeLock />,
+    text: 'Analytics Dashboard',
+    description:
+      'Gain valuable insights into user interactions and behavior within your VR applications with an integrated analytics dashboard.',
+  },
+];
+
+export const checklistItems = [
+  {
+    title: 'Code merge made easy',
+    description:
+      'Track the performance of your VR apps and gain insights into user behavior.',
+  },
+  {
+    title: 'Review code without worry',
+    description:
+      'Track the performance of your VR apps and gain insights into user behavior.',
+  },
+  {
+    title: 'AI Assistance to reduce time',
+    description:
+      'Track the performance of your VR apps and gain insights into user behavior.',
+  },
+  {
+    title: 'Share work in minutes',
+    description:
+      'Track the performance of your VR apps and gain insights into user behavior.',
+  },
+];
+
+export const pricingOptions = [
+  {
+    title: 'Free',
+    price: '$0',
+    features: [
+      'Private board sharing',
+      '5 Gb Storage',
+      'Web Analytics',
+      'Private Mode',
+    ],
+  },
+  {
+    title: 'Pro',
+    price: '$10',
+    features: [
+      'Private board sharing',
+      '10 Gb Storage',
+      'Web Analytics (Advance)',
+      'Private Mode',
+    ],
+  },
+  {
+    title: 'Enterprise',
+    price: '$200',
+    features: [
+      'Private board sharing',
+      'Unlimited Storage',
+      'High Performance Network',
+      'Private Mode',
+    ],
+  },
+];
+
+export const resourcesLinks = [
+  { to: '/Getting-Started', text: 'Getting Started' },
+  { to: '/Documentation', text: 'Documentation' },
+  { to: '/Tutorials', text: 'Tutorials' },
+  { to: '/API-Reference', text: 'API Reference' },
+  { to: '/Community-Forums', text: 'Community Forums' },
+];
+
+export const platformLinks = [
+  { to: 'Features', text: 'Features' },
+  { to: 'Supported-Devices', text: 'Supported Devices' },
+  { to: 'System-Requirements', text: 'System Requirements' },
+  { to: 'Downloads', text: 'Downloads' },
+  { to: 'Release-Notes', text: 'Release Notes' },
+];
+
+export const communityLinks = [
+  { to: 'Events', text: 'Events' },
+  { to: 'Meetups', text: 'Meetups' },
+  { to: 'Conferences', text: 'Conferences' },
+  { to: 'Hackathons', text: 'Hackathons' },
+  { to: 'Jobs', text: 'Jobs' },
+];
+
+export const heroLinks = [
+  { to: 'start-for-free', text: 'start for free' },
+  { to: 'documentation', text: 'documentation' },
+];
