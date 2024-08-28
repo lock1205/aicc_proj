a1cd7651454d463bbb77390a14d5439f3577c19e
lollipopwyn <rwewine@gmail.com>
2024-08-28 11:40:17 +0900

    나라_0828_back 협의서 내용 get,post 구축_잘 작동되는 것 확인

--- back/controllers/getTasks.js
+++ back/controllers/getTasks.js
@@ -2,10 +2,10 @@ const database = require('../database/database');
 
 exports.getTasks = async (req, res) => {
   try {
-    const allTasks = await pool.query('SELECT * FROM agreement'); // agreement테이블에서 모든 행을 선택
-    res.json(allTasks.rows);
+    const allTasks = await database.query('SELECT * FROM agreement'); // agreement테이블에서 모든 행을 선택
+    // res.json(allTasks.rows);
 
-    return res.status(201).json({ message: 'get Info successfully' });
+    return res.status(201).json(allTasks.rows);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
--- back/controllers/postTasks.js
+++ back/controllers/postTasks.js
@@ -4,27 +4,27 @@ const database = require('../database/database');
 exports.postTasks = async (req, res) => {
   try {
     const values = [
-      req.body.arg_num,
-      // req.body.key,
+      req.body.key,
       req.body.company_name,
       req.body.level,
       req.body.master_name,
       req.body.master_tel,
       req.body.end_date,
       req.body.sum_money,
-      // req.body.arg_edit,
-      // req.body.arg_del,
-      // req.body.arg_cancel,
       req.body.ai_data,
       req.body.ai_media,
       req.body.ai_lang,
+      req.body.title,
+      req.body.status,
+      req.body.description,
+      req.body.ai_image,
     ];
     await database.query(
-      'INSERT INTO agreement(arg_num,company_name,level,master_name,master_tel,end_date,sum_money,ai_data,ai_media,ai_lang) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)',
+      'INSERT INTO agreement(user_key,company_name,level,master_name,master_tel,end_date,sum_money,ai_data,ai_media,ai_lang,title,status,description,ai_image) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)',
       values
     );
 
-    return res.status(201).json({ message: 'agreement submit successfully' });
+    return res.status(201).json({ message: 'Agreement post successfully' });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
